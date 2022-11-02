e8e111b143b237e43f35296b59330be473f5362c
eizo <wjc2014123@outlook.com>
2022-11-02 21:43:48 +0800

    DeleteAllView done.

--- app/user/delete.py
+++ app/user/delete.py
@@ -21,7 +21,7 @@ class DeleteOneView(Resource):
         args = parser.parse_args()
         userid = User.decode_auth_token(args['token'])
         userwordrel = Userwordrel.query.filter_by(user_id=userid, word_id=word_id).first()
-        if userwordrel is not None:
+        if userwordrel:
             try:
                 path = userwordrel.snd_path
                 db.session.delete(userwordrel)
@@ -32,9 +32,9 @@ class DeleteOneView(Resource):
                 db.session.rollback()
                 current_app.logger.error(e)
                 return {"status": 404, "message": "数据库查询异常"}, 404
-            return {"status": 200, "message": "delete successful"}, 200
+            return {"status": 200, "message": "delete successfully"}, 200
         else:
-            return {'status': 403, 'message': 'no this audio'}, 403
+            return {'status': 403, 'message': 'Do not upload this audio!'}, 403
 
 
 class DeleteAllView(Resource):
@@ -44,22 +44,22 @@ class DeleteAllView(Resource):
     def post(self):
         args = parser.parse_args()
         userid = User.decode_auth_token(args['token'])
-        path = []
         userwordrel = Userwordrel.query.filter_by(user_id=userid).all()
-        if userwordrel is not None:
-            # try:
-            for item in userwordrel:
-                path.append(item.snd_path)
-            current_app.logger.info(userid)
-            current_app.logger.info(path)
-            # except Exception as e:
-            #     # 数据库出错回滚
-            #     db.session.rollback()
-            #     current_app.logger.error(e)
-            #     return {"status": 404, "message": "数据库查询异常"}, 404
-            return {"status": 200, "message": 'userwordrel'}, 200
+        current_app.logger.info(userwordrel)
+        if userwordrel:
+            try:
+                for item in userwordrel:
+                    db.session.delete(item)
+                    db.session.commit()
+                    os.remove(item.snd_path)
+            except Exception as e:
+                # 数据库出错回滚
+                db.session.rollback()
+                current_app.logger.error(e)
+                return {"status": 404, "message": "数据库查询异常"}, 404
+            return {"status": 200, "message": 'delete successfully'}, 200
         else:
-            return {'status': 403, 'message': 'no this audio'}, 403
+            return {'status': 403, 'message': 'Do not upload audio yet!'}, 403
 
 
 user_api.add_resource(DeleteOneView, '/<word_id>/delete')
