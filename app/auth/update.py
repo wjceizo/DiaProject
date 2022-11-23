8d8685a61dbaaeeb86931d59e965a33bef0cb5da
eizo <wjc2014123@outlook.com>
2022-11-23 14:08:59 +0800

    update information

--- app/auth/update.py
+++ app/auth/update.py
@@ -5,7 +5,7 @@ from flask_restful import reqparse
 from flask import current_app
 
 parser = reqparse.RequestParser()
-parser.add_argument('username', type=str, required=True, location='json', help='用户名不能为空')
+parser.add_argument('token', type=str, required=True, location='json', help='token不能为空')
 parser.add_argument('password', type=str, location='json', help='密码不能为空')
 parser.add_argument('newpassword', type=str, location='json', help='密码不能为空')
 parser.add_argument('email', type=str, location='json', help='电子邮箱重复')
@@ -20,7 +20,7 @@ class UpdatePasswordView(Resource):
 
     def post(self):
         args = parser.parse_args()
-        user = User.query.filter_by(username=args['username']).first()
+        user = User.query.filter_by(id=User.decode_auth_token(args['token'])).first()
         if user is not None:
             if user.verify_password(args['password']):
                 user.verified = True
@@ -41,8 +41,34 @@ class UpdatePasswordView(Resource):
 
 
 class UpdateInformationView(Resource):
+
     def get(self):
-        return {'help': 'UpdateInformationView'}, 200
+        args = parser.parse_args()
+        user = User.query.filter_by(id=User.decode_auth_token(args['token'])).first()
+        return {'id': user.id, 'email': user.email, 'username': user.username,
+                'name': user.name, 'location': user.location, 'lang': user.lang}, 200
+
+
+    def post(self):
+        args = parser.parse_args()
+        user = User.query.filter_by(id=User.decode_auth_token(args['token'])).first()
+        if user is not None:
+            user.verified = True
+            user.email = args['email']
+            user.name = args['name']
+            user.location = args['location']
+            user.lang = args['lang']
+            try:
+                db.session.commit()  # SQLAlchemy用
+            except Exception as e:
+                # 数据库出错回滚
+                db.session.rollback()
+                current_app.logger.error(e)
+                return {"status": 404, "message": "数据库查询异常"}, 404
+        else:
+            return {'status': 403, 'message': '不存在该用户'}, 403
+
+        return {'status': 200, 'message': '用户信息更新成功'}, 200
 
 
 auth_api.add_resource(UpdatePasswordView, '/update/password')
