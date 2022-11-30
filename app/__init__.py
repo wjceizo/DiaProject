96de37846ff8229669b6f0db26055165de744de3
wjceizo <wjc2014123@outlook.com>
2022-11-30 16:24:31 +0800

    delete some useless package

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from flask_mail import Mail
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
-bootstrap = Bootstrap()
-mail = Mail()
-moment = Moment()
+
 db = SQLAlchemy()
 
 
@@ -17,11 +12,8 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    # bootstrap.init_app(app)
-    mail.init_app(app)
-    moment.init_app(app)
     db.init_app(app)
-    # login_manager.init_app(app)
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
