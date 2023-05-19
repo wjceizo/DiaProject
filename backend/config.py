73b44bd50a595586f16ca2ddf7c42d0be6e9a5f3
eizo <wjc2014123@outlook.com>
2023-05-19 22:47:17 +0800

    /api 2.0

--- backend/config.py
+++ backend/config.py
@@ -18,10 +18,10 @@ class Config:
     API_TITLE = "Dialect Map REST API"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
-    OPENAPI_URL_PREFIX = "/"
-    OPENAPI_REDOC_PATH = "/api/redoc"
+    OPENAPI_URL_PREFIX = "/api"
+    OPENAPI_REDOC_PATH = "/redoc"
     OPENAPI_REDOC_URL = "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"
-    OPENAPI_RAPIDOC_PATH = "/api/rapidoc"
+    OPENAPI_RAPIDOC_PATH = "/rapidoc"
     OPENAPI_RAPIDOC_URL = "https://unpkg.com/rapidoc/dist/rapidoc-min.js"
     OPENAPI_RAPIDOC_CONFIG = {"theme": "dark"}
 
